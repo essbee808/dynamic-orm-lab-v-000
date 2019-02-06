115b0af23552a087e14722c54e249cadb37f42a6
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-06 01:57:59 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,7 +58,7 @@ class InteractiveRecord
   def self.find_by(column_name)
     binding.pry
     el_key = column_name.keys[0]
-    el_value = 
+    el_value = column_name[el_key]
     sql = "SELECT * FROM #{self.table_name} WHERE #{el_key.to_s} = #{el_value.to_s}"
     DB[:conn].execute(sql)
   end
