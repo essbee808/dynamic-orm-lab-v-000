1203f41b98d0a51e7e22db1d8c7978640602305d
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-06 02:04:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,6 +58,7 @@ class InteractiveRecord
     el_key = column_name.keys[0]
     el_value = column_name[el_key]
     sql = "SELECT * FROM #{self.table_name} WHERE #{el_key.to_s} = '#{el_value.to_s}'"
+    #sql = "SELECT * FROM ? WHERE ? = ?"
     DB[:conn].execute(sql)
   end
 end
